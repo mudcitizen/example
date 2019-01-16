3a2631b06285f94e8406e194a0013674312f6191
MudCitizen <mudcitizen@gmail.com>
2019-01-16 05:52:45 -0500

    Host Elememt Binding - Broken

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,7 +1,8 @@
 // Import a few directives
 import {
-    Directive, ElementRef, Attribute, Input, Output,
-    OnChanges, SimpleChange, EventEmitter
+    Directive, Input, Output,
+    EventEmitter,
+    HostBinding, HostListener
 } from "@angular/core";
 
 import { Product } from "./product.model";
@@ -9,38 +10,20 @@ import { Product } from "./product.model";
 @Directive({
     selector: "[pa-attr]",
 })
-export class PaAttrDirective implements OnChanges {
+export class PaAttrDirective {
 
     @Input("pa-attr")
+    @HostBinding("class")
     bgClass: string;
 
     @Input("pa-product")
     theProduct: Product;
 
     @Output("pa-category")
-    /* Template binding target is (pa-category) 
-       Attr does click.emit(<payload>)
-       ie pa-category is the templates alias for the click
-       property
-    */
     click = new EventEmitter<string>();
 
-    constructor(private element: ElementRef) {
-        this.element.nativeElement.addEventListener("click", e => {
-            if (this.theProduct != null) {
-                this.click.emit(this.theProduct.category);
-            }
-        });
-    }
-
-    ngOnChanges(changes: { [property: string]: SimpleChange }) {
-        let change = changes["bgClass"];
-        let classList = this.element.nativeElement.classList;
-        if (!change.isFirstChange() && classList.contains(change.previousValue)) {
-            classList.remove(change.previousValue);
-        }
-        if (!classList.contains(change.currentValue)) {
-            classList.add(change.currentValue);
-        }
+    @HostBinding("click")
+    triggerCustomEvent() {
+        this.click.emit(this.theProduct.category);
     }
 }
