dcc8b22c87b964f819d278f32285e3ea1853e040
MudCitizen <mudcitizen@gmail.com>
2019-01-19 10:59:55 -0500

    Chapter 15 - Export Directive for use as template variable

--- src/app/twoway.directive.ts
+++ src/app/twoway.directive.ts
@@ -6,26 +6,20 @@ import {
 import {ApplicationRef} from "@angular/core";
 
 @Directive({
-    selector: "input[paModel]"
+    selector: "input[paModel]",
+    exportAs: "paModel"
 })
 export class PaModel implements OnChanges  {
 
+    direction:string;
     private _modelProperty : string;
     @Input("paModel")
-    /* Template has the following
-      <input [paModel]="newProduct.name" 
-             (paModelChange)="newProduct.name=$event" />
-    */
-   // newProduct.name updates this.modelProperty
     set modelProperty( value: string) {
         this._modelProperty = (value);
         console.log("modelProperty setter",this._modelProperty);
     }
     get modelProperty():string {return this._modelProperty;}
 
-    /* this.fieldValue updates template <input> value 
-    [value]="fieldValue"
-    */
     @HostBinding("value")
     fieldValue: string = "";
 
@@ -38,18 +32,17 @@ export class PaModel implements OnChanges  {
         let change = changes["modelProperty"];
         if (change.currentValue != this.fieldValue) {
             this.fieldValue = changes["modelProperty"].currentValue || "";
+            this.direction = "Model";
         }
     }
 
     @Output("paModelChange")
     update = new EventEmitter<string>();
 
-    // When the Host Element's (input) event occurs the updateValue() 
-    // method is executed which fires the (paModelChange) method - which
-    // updates newProduct.name
     @HostListener("input", ["$event.target.value"])
     updateValue(newValue: string) {
         this.fieldValue = newValue;
         this.update.emit(newValue);
+        this.direction = "Element"
     }
 }
