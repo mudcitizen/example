8488391e1370897a74fa40a3c0befc88eea4b11f
MudCitizen <mudcitizen@gmail.com>
2019-01-22 20:39:04 -0500

    Chapter 17 - Before Projecting Host Element Content

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -1,7 +1,31 @@
-import { Component } from "@angular/core";
+import { Component,Input, ApplicationRef } from "@angular/core";
+import { Model } from "./repository.model"
+import { Product } from "./product.model";
+
 @Component({
     selector: "paProductTable",
     templateUrl: "productTable.component.html"
 })
 export class ProductTableComponent {
+    
+    @Input("model")
+    dataModel : Model;
+
+    constructor(appRef:ApplicationRef){
+        (<any>window).appRef = appRef;
+        (<any>window).prodTableComp = this;
+
+    }
+
+    showTable : boolean;   
+
+    getProduct(key: number): Product {
+        return this.dataModel.getProduct(key);
+    }
+    getProducts(): Product[] {
+        return this.dataModel.getProducts();
+    }
+    deleteProduct(key: number) {
+        this.dataModel.deleteProduct(key);
+    }
 }
