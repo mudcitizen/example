d3bd58be420a88c4bdcba4a86fb9582d1d83b1c4
MudCitizen <mudcitizen@gmail.com>
2019-02-09 11:12:43 -0500

    Date and JSON pipes

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -9,6 +9,16 @@ import { PaCellColor } from "./cellColor.directive";
     templateUrl: "productTable.component.html"
 })
 export class ProductTableComponent {
+
+    private _taxRate:string;
+    set taxRate(tr:string) {
+        this._taxRate = tr.trim();
+    }
+    get taxRate() : string {return this._taxRate;}
+    
+    dateObject: Date = new Date(2020, 1, 20);
+    dateString: string = "2020-02-20T00:00:00.000Z";
+    dateNumber: number = 1582156800000;
     
     @Input("model")
     dataModel : Model;
