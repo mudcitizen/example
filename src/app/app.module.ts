3be4b76d7c8e9be7494bdef0130035612d641b77
MudCitizen <mudcitizen@gmail.com>
2019-02-18 20:39:00 -0500

    Service Provider - FactoryProvider with deps array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,26 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { PaAttrDirective } from "./attr.directive";
 import { PaModel } from "./twoway.directive";
 import { PaStructureDirective } from "./structure.directive";
-import { PaIteratorDirective } from "./iterator.directive"; 
+import { PaIteratorDirective } from "./iterator.directive";
 import { PaCellColor } from "./cellColor.directive";
-import { PaCellColorSwitcher } from "./cellColorSwitcher.directive"; 
+import { PaCellColorSwitcher } from "./cellColorSwitcher.directive";
 import { ProductTableComponent } from "./productTable.component";
-import { ProductFormComponent } from "./productForm.component"; 
+import { ProductFormComponent } from "./productForm.component";
 import { PaToggleView } from "./toggleView.component";
 import { PaAddTaxPipe } from "./addTax.pipe";
 import { PaCategoryFilterPipe } from "./categoryFilter.pipe";
 import localeFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common';
-import { PaDiscountDisplayComponent } from "./discountDisplay.component";                                          
+import { PaDiscountDisplayComponent } from "./discountDisplay.component";
 import { PaDiscountEditorComponent } from "./discountEditor.component";
-import { DiscountService} from "./discount.service";
+import { DiscountService } from "./discount.service";
 import { PaDiscountPipe } from "./discount.pipe";
 import { PaDiscountAmountDirective } from "./discountAmount.directive"
 import { Model } from "./repository.model";
 import { SimpleDataSource } from "./datasource.model";
 import { LogService, LogLevel } from "./log.service";
-import { SpecialLogService } from  "./speciallogservice.service"
+import { LOG_LEVEL } from "./log.service";
+import { SpecialLogService } from "./speciallogservice.service"
 
 let logger = new LogService();
 logger.minimumLevel = LogLevel.DEBUG;
@@ -44,11 +45,20 @@ logger.minimumLevel = LogLevel.DEBUG;
         PaDiscountPipe,
         PaDiscountAmountDirective,
         PaStructureDirective],
-       providers: [DiscountService, 
-        SimpleDataSource, 
-        { provide: LogService, useValue: logger },
+    providers: [DiscountService,
+        SimpleDataSource,
+        { provide: LOG_LEVEL, useValue: LogLevel.DEBUG },
+        {
+            provide: LogService, 
+            deps: [LOG_LEVEL], 
+            useFactory: (level) => {
+                let logger = new LogService();
+                logger.minimumLevel = level;
+                return logger;
+            }
+        },
         Model
     ],
     bootstrap: [ProductComponent]
 })
-export class AppModule {}
+export class AppModule { }
