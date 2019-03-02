919715b2ada8ce2d6dbcf89213552a981fc21f93
MudCitizen <mudcitizen@gmail.com>
2019-03-01 19:56:14 -0500

    Service Provider - @Host and @Optional in directive

--- src/app/valueDisplay.directive.ts
+++ src/app/valueDisplay.directive.ts
@@ -1,12 +1,12 @@
-import { Directive, InjectionToken, Inject, HostBinding} from "@angular/core";
+import { Directive, InjectionToken, Inject, HostBinding, Host, Optional} from "@angular/core";
 export const VALUE_SERVICE = new InjectionToken("value_service");
 
 @Directive({
     selector: "[paDisplayValue]"
 })
 export class PaDisplayValueDirective {
-    constructor( @Inject(VALUE_SERVICE) serviceValue: string) {
-        this.elementContent = serviceValue;
+    constructor( @Inject(VALUE_SERVICE) @Host() @Optional() serviceValue: string) {
+        this.elementContent = serviceValue || "No Value";
     }
     @HostBinding("textContent")
     elementContent: string;
